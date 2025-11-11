c7c79fcc45f0cc9d1c95e1ad4117d916f4c586e3
Nikita Parkovskyi <143649497+K1taSun@users.noreply.github.com>
2025-11-11 01:49:32 +0100

    Wstępna struktura UI i czyszczenie projektu
    Usunięto domyślny kod aplikacji Flutter.

---
+++ App/phoneapp/lib/screens/recordings_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RecordingsScreen extends StatelessWidget {
+  const RecordingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Recordings'),
+    );
+  }
+}
