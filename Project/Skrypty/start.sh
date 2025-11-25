b77a1c976b493ecad60bcfcbaa780186f683a6e4
Nikita Parkovskyi <143649497+K1taSun@users.noreply.github.com>
2025-11-25 22:55:44 +0100

    Initial commit add Project gitignore

---
+++ Project/Skrypty/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /home/niki/Desktop/Project/Skrypty/gdrive_env/bin/activate 
+cd /home/niki/Desktop/Project/Skrypty
+python3 Projekt.py >> /home/niki/Desktop/Project/Skrypty/log.txt 2>&1
